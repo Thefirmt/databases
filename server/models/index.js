49d75ae76d3c5c0591dbfa114e57c1f21b7cdd13
thefirmt <thefirmt@gmail.com>
2019-08-07 21:30:13 -0700

    finished almost all of the functionality for post and get for the first test case with users

--- server/models/index.js
+++ server/models/index.js
@@ -3,31 +3,31 @@ var db = require('../db');
 module.exports = {
   messages: {
     get: function (callback) {
-      db.sql_connection.connect();
-      db.sql_connection.query('SELECT * FROM messages', function(err, dataRows) {
+      db.sqlConnection.connect();
+      db.sqlConnection.query('SELECT * FROM messages', function(err, dataRows) {
         if (err) {
           throw error('bad');
-          db.sql_connection.end();
+          db.sqlConnection.end();
         } else {
           callback(dataRows);
-          db.sql_connection.end();
+          db.sqlConnection.end();
         }
       });
     }, // a function which produces all the messages
     post: function (data, callback) {
       // const {username, room, msg} = data;
-      db.sql_connection.connect();
+      db.sqlConnection.connect();
       var username = data.username;
       var room = data.roomname;
       var msg = data.message;
 
-      db.sql_connection.query(`INSERT INTO messages (id, message, created_on, room_id, user_id) VALUES (null, ${msg}, ${Date.now}, ${room}, ${username})`, function(err, data) {
+      db.sqlConnection.query(`INSERT INTO messages (id, message, created_on, room_id, user_id) VALUES (null, ${msg}, ${Date.now}, ${room}, ${username})`, function(err, data) {
         if (err) {
           throw error('bad');
-          db.sql_connection.end();
+          db.sqlConnection.end();
         } else {
           console.log('posted a message!');
-          db.sql_connection.end();
+          db.sqlConnection.end();
         }
       });
     } // a function which can be used to insert a message into the database
@@ -39,15 +39,15 @@ module.exports = {
 
     },
     post: function (data, callback) {
-      db.sql_connection.connect();
+      db.sqlConnection.connect();
       var username = data.username;
       db.query(`INSERT INTO users (id, username) VALUES (null, ${username})`, function(err, data) {
         if (err) {
           throw error('bad');
-          db.sql_connection.end();
+          db.sqlConnection.end();
         } else {
           console.log('posted a message!');
-          db.sql_connection.end();
+          db.sqlConnection.end();
         }
       });
     }
