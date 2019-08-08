0ea6df34b6fd2b166263b346376925b296a2bf77
thefirmt <thefirmt@gmail.com>
2019-08-07 21:35:47 -0700

    finished almost all of the functionality for post and get for the first test case with users

--- server/db/index.js
+++ server/db/index.js
@@ -4,7 +4,7 @@ var mysql = require('mysql');
 // You will need to connect with the user "root", no password,
 // and to the database "chat".
 
-exports.sqlConnection = mysql.createPool({
+exports.sqlConnection = mysql.createConnection({
   host: 'localhost',
   user: 'student',
   password: 'student',
